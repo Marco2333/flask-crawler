2621890573164d3757d6bb960288ada95bbe62f8
mrwhoareyou <2359146115@qq.com>
2017-11-21 23:10:31 +0800

    basicinfo_crawler.py

--- app/controller/crawler/basicinfo_crawler.py
+++ app/controller/crawler/basicinfo_crawler.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import gc
 import time
 import threading
 
@@ -63,6 +64,7 @@ class BasicinfoCrawler:
 		if user_id == None and screen_name == None:
 			return None
 
+		
 		return self.get_api().GetUser(user_id = user_id,
 									  screen_name = screen_name,
 									  include_entities = include_entities)
@@ -102,11 +104,9 @@ class BasicinfoCrawler:
 					  include_entities = True):
 
 		wrapper_func = handle_exception(self.get_user)
-
+		
 		user = wrapper_func(user_id = user_id, screen_name = screen_name, include_entities = include_entities)
 
-		del wrapper_func
-		
 		user and self.save_user(user, table_name)
 
 
@@ -188,9 +188,14 @@ class BasicinfoCrawler:
 			return
 
 		try:
-			name = user.name.replace("'","\\'")
-			location = user.location.replace("'","\\'") if user.location else ''
-			description = user.description.replace("'","\\'") if user.description else ''
+			name = user.name.replace('\\','\\\\').replace("'","\\'").replace(':','\\:')
+			location = user.location.replace('\\','\\\\').replace("'","\\'").replace(':','\\:') if user.location else ''
+			description = user.description.replace('\\','\\\\').replace("'","\\'").replace(':','\\:') if user.description else ''
+
+			time_zone = user.time_zone if user.time_zone else ''
+			utc_offset = user.utc_offset if user.utc_offset else ''
+			profile_banner_url = user.profile_banner_url if user.profile_banner_url else ''
+
 			protected = 1 if user.protected else 0
 			verified = 1 if user.verified else 0
 			geo_enabled = 1 if user.geo_enabled else 0
@@ -200,10 +205,10 @@ class BasicinfoCrawler:
 					followers_count, favourites_count, lang, protected, time_zone, verified, utc_offset, geo_enabled, listed_count,
 					default_profile_image, profile_background_color, profile_sidebar_fill_color, profile_image_url, profile_banner_url, crawler_date) VALUES
 					('%s', '%s', '%s', '%s', '%s', '%s', %d, %d, %d, %d, '%s', %d, '%s', %d, '%s', %d, %d, %d,
-					'%s', '%s', '%s', '%s', '%s')""" % (table_name, user.id, user.screen_name, name, location, user.created_at, description, user.statuses_count, \
-					user.friends_count, user.followers_count, user.favourites_count, user.lang, protected, user.time_zone, verified, \
-					user.utc_offset, geo_enabled, user.listed_count, default_profile_image, user.profile_background_color, \
-					user.profile_sidebar_fill_color, user.profile_image_url, user.profile_banner_url, time.strftime('%Y-%m-%d',time.localtime(time.time()))) 
+					'%s', '%s', '%s', '%s', '%s')""" % (table_name, user.id, user.screen_name, name, location, user.created_at, description, \
+					user.statuses_count, user.friends_count, user.followers_count, user.favourites_count, user.lang, protected, time_zone, verified, \
+					utc_offset, geo_enabled, user.listed_count, default_profile_image, user.profile_background_color, \
+					user.profile_sidebar_fill_color, user.profile_image_url, profile_banner_url, time.strftime('%Y-%m-%d',time.localtime(time.time()))) 
 
 			sql = sql.encode("utf-8").decode("latin1")
 		except Exception as e:
@@ -212,7 +217,7 @@ class BasicinfoCrawler:
 
 		ctx = app.app_context()
 		ctx.push()
-
+		
 		try:
 			db.session.execute(sql)
 			db.session.commit()
@@ -221,6 +226,9 @@ class BasicinfoCrawler:
 			return
 
 		ctx.pop()
+
+		del user
+		gc.collect()
 
 
 if __name__ == '__main__':
